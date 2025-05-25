845956f62a06cc69025214009a5c62d5579ec803
Thomas Mario <thmapneadesigner@gmail.com>
2025-05-25 10:47:49 +0200

    Update Double_Inverted_Operational_Amplifier_Circuit.m

--- Electronic System/Double_Inverted_Operational_Amplifier_Circuit.m
+++ Electronic System/Double_Inverted_Operational_Amplifier_Circuit.m
@@ -25,4 +25,12 @@ clc; clear; close all;
 %
 %we want to anlayze the following circuit that contains two inverted
 %operational amplifiers, five resistances, three, capacitators and three
-%inductors
+%inductors. The i-s-u representation of the system is the following:
+% ẋ1(t) = -1/(R1*C1)*x1(t) + 1/(R1*C1)*u(t)
+% ẋ2(t) = -R2/(R1*L1)*x1(t) - R2/L1*x2(t) + R2/(R1*L1)*u(t)
+% ẋ3(t) = R2/(R1*R3*C2)*x1(t) + R2/(R3*C2)*x2(t) - 1/(R3*C2)*x3(t) - R2/(R1*R3*C2)*u(t)
+% ẋ4(t) = R2*R4/(R1*R3*L2)*x1(t) + R2*R4/(R3*L2)*x2(t) - R4/(R3*L2)*x3(t) - R4/L2*x4(t) - R2*R4/(R1*R3*L2)*u(t)
+% ẋ5(t) = -R2*R4/(R1*R3*R5*C3)*x1(t) - R2*R4/(R3*R5*C3)*x2(t) + R4/(R3*R5*C3)*x3(t) + R4/(R5*C3)*x4(t) - 1/(R5*C3)*x5(t) + R2*R4/(R1*R3*R5*C3)*u(t)
+% y(t) = -R2*R4/(R1*R3)*x1(t) - R2*R4/R3*x2(t) + R4/R3*x3(t) + R4*x4(t) - x5(t) + R2*R4/(R1*R3)*u(t)
+% The resulting system is dynamic, continuous-time, LTI, SISO, proper, of fifth order
+%we will proceed with a quick analysis of the circuit
